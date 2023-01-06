b109896cd32c99f49c1ba8a6f998670b82fd5802
Mikhail Turilov <turilovm@gmail.com>
2023-01-06 20:54:47 +0300

    added test files

---
+++ src/test/java/tests/ServiceTest.java
@@ -0,0 +1,47 @@
+package tests;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+
+public class ServiceTest {
+    @BeforeEach
+    void setUpPage(){
+        Selenide.open("https://www.otpbank.ru/");
+//        Configuration.holdBrowserOpen = true;
+    }
+    @Test
+    void serviceTest(){
+        step("Выбираем вкладку Услуги", () ->{
+            $(".main-menu").find(byText("Услуги")).click();
+            $(".content__right").shouldHave(Condition.text("Банковские услуги"));
+        });
+        step("В меню слева убедиться, что есть подменю Аренда сейфов, кликнуть на эту ссылку", () ->{
+            $(".content__left").shouldHave(Condition.text("Аренда сейфов"));
+            $(".content__left").find(byText("Аренда сейфов")).click();
+            $(".content__right").shouldBe(Condition.text("Аренда сейфов и ячеек"));
+        });
+    }
+    @Test
+    void testDownloadFile() throws Exception {
+        step("Заходим на вкладку Услуги -> Аренда сейфов", () ->{
+            $(".main-menu").find(byText("Услуги")).click();
+            $(".content__left").find(byText("Аренда сейфов")).click();
+        });
+        File fileDownloaded = $(byXpath("//a[@href ='/f/retail/safes/pravila_fl.pdf']")).download();
+
+
+
+    }
+
+}
