d7b83534c83a25eacef471a5928ae1f12c4ebd80
Mikhail Turilov <turilovm@gmail.com>
2023-01-06 21:30:53 +0300

    added test files

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -14,8 +14,8 @@ public class BaseTest {
     @BeforeAll
     static void setUp(){
         Selenide.clearBrowserCookies();
-//        Configuration.timeout = 5000;
-//        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.timeout = 5000;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         Configuration.holdBrowserOpen = true;
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("browserName", "chrome");
